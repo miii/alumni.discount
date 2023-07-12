30810804caaeb5b58d8023404a8034c11644e66a
Jacob Andersson <jacob.m.andersson@gmail.com>
2023-07-12 23:56:55 +0200

    fix: prevent ssrf

--- src/server/api/logo.ts
+++ src/server/api/logo.ts
@@ -2,11 +2,27 @@ import sharp from 'sharp'
 
 export default defineEventHandler(async (event) => {
   const imageSrc = getQuery(event).src as string
+  
+  try {
+    // Validate image URL
+    const url = new URL(imageSrc)
+    // Validate CDN to prevent SSRF
+    if (!['img.meccdn.com', 'www.studentkortet.se'].includes(url.hostname))
+      throw new Error('Invalid hostname')
+  } catch (error) {
+    throw createError({
+      status: 400,
+      message: 'Invalid image URL',
+      cause: error,
+    })
+  }
+
   const imageBuffer = await $fetch<ArrayBuffer>(imageSrc, { responseType: 'arrayBuffer' })
 
   const image = sharp(imageBuffer).trim()
   const buffer = await image.toBuffer()
 
   event.node.res.setHeader('Cache-Control', 'public, max-age=31536000, immutable')
+
   return buffer
 })
