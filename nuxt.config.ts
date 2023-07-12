a00374a3fa21c7d6de19a0e7edca22ce7e839877
Jacob Andersson <jacob.m.andersson@gmail.com>
2023-07-13 00:54:33 +0200

    fix: use prompt mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
     },
   },
   pwa: {
-    registerType: 'autoUpdate',
+    registerType: 'prompt',
     manifest: {
       name: title,
       short_name: title,
