92035171e732eb7dfc309cbf1952c5e019a39a15
Jacob Andersson <jacob.m.andersson@gmail.com>
2023-10-04 01:07:21 +0200

    fix: remove version from footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,8 +7,7 @@ const shortVersion = version.replace(/\.0$/, '')
 
 <template>
   <a href="https://www.github.com/miii" target="_blank" rel="noopener noreferrer"
-    class="text-center text-xs opacity-30 fixed bottom-6 font-semibold flex gap-2 items-center hover:opacity-50">
+    class="text-center text-xs opacity-20 fixed bottom-6 font-semibold flex gap-2 items-center hover:opacity-40 transition-opacity">
     <GithubLogo class="w-4" />
-    Alumnirabatt v{{ shortVersion }}
   </a>
 </template>
